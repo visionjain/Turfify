8b7f052fd44a09025b7fc16386ed003b938fb659
Vision Jain <visionjain118@gmail.com>
2024-03-18 03:35:22 +0530

    sdfsdfhsdfd

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -24,7 +24,7 @@ const Nav = () => {
 
   const router = useRouter();
   const loginredirect = () => {
-    router.push('/logingate');
+    router.push('/login');
   };
 
   const inputRef = useRef<HTMLInputElement | null>(null);
