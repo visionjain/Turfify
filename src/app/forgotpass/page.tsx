cff4de7465ec27e8c2492afb5b5660a98d0bd49b
Vision Jain <visionjain118@gmail.com>
2024-03-18 05:17:49 +0530

    yupbutton

--- src/app/forgotpass/page.tsx
+++ src/app/forgotpass/page.tsx
@@ -1,11 +1,15 @@
+import DarkModeButton from '@/components/darkmode/page'
 import React from 'react'
 
 const Forgotpass = () => {
-  return (
-    <div className='flex items-center justify-center h-screen text-5xl'>
-      Bana Raha hu BSDK
-    </div>
-  )
+    return (
+        <div>
+            <DarkModeButton/>
+            <div className='flex items-center justify-center h-screen text-5xl'>
+                Bana Raha hu BSDK
+            </div>
+        </div>
+    )
 }
 
 export default Forgotpass
