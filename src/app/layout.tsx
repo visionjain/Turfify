b1f8a59d4b6594569776e523a016d0d7f75d8410
Vision Jain <visionjain118@gmail.com>
2024-03-18 02:30:44 +0530

    hii

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Turfify",
@@ -13,9 +14,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const apiKey = process.env.PLACE_API;
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {apiKey && (
+          <Script
+            type="text/javascript"
+            src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&loading=async&libraries=places`}
+            strategy="beforeInteractive"
+          />
+        )}
+      </body>
     </html>
   );
 }
